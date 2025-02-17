f613069753b1a2f9c30886fd8350cc920c241ec9
Sylwia <sylwiamlr@gmail.com>
2025-02-17 15:04:14 +0100

    Spring Cloud Gateway - full version

---
+++ gateway-service/src/main/java/org/sylwia/GatewayConfig.java
@@ -0,0 +1,20 @@
+package org.sylwia;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("customer", r -> r.path("/api/v1/customers/**")
+                        .uri("lb://CUSTOMER"))
+                .route("fraud", r -> r.path("/api/v1/fraud-check/**")
+                        .uri("lb://FRAUD"))
+                .build();
+    }
+}
--- gateway-service/src/main/java/org/sylwia/GatewayServerApplication.java
+++ gateway-service/src/main/java/org/sylwia/GatewayServerApplication.java
@@ -3,23 +3,10 @@ package org.sylwia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayServerApplication {
-
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("customer", r -> r.path("/customer/**")
-                        .filters(f -> f.stripPrefix(1))
-                        .uri("lb://CUSTOMER"))
-                .build();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
     }
